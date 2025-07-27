6b8acffcbcab3e215b57cad27eae59b1ee34bba1
luizamiotto <luiza.miotto@ludfor.com.br>
2025-07-27 20:52:36 -0300

    Adjusting flexDirection

--- app/components/statementComponents/Statement.tsx
+++ app/components/statementComponents/Statement.tsx
@@ -18,6 +18,7 @@ export default function Statement() {
 		}}>
 			<Box sx={{
 				display: 'flex',
+				flexDirection: 'column',
 				alignItems: 'center',
 				justifyContent: 'space-between',
 			}}>
